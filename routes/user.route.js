c1cac0ffca986f3e47e265f5dffe26b983e88b43
MuhammadBorhan <0317371981@student.wub.edu.bd>
2023-05-07 15:22:45 +0600

    login api created

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 
 router.route("/signup").post(userController.postUser);
+router.route("/login").post(userController.loginPost);
 
 module.exports = router;
