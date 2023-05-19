df5396ca5dbc55cb123361e7541c281f4ab4e658
MuhammadBorhan <0317371981@student.wub.edu.bd>
2023-05-19 22:39:08 +0600

    so far so good

--- controllers/region.controller.js
+++ controllers/region.controller.js
@@ -74,3 +74,22 @@ exports.updateRegion = async (req, res) => {
     });
   }
 };
+
+exports.deleteRegion = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const region = await Regions.deleteOne({ _id: id });
+
+    res.status(200).json({
+      status: "Success",
+      message: "Data delete successfully!",
+      data: region,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "Data is not deleted",
+      error: error.message,
+    });
+  }
+};
