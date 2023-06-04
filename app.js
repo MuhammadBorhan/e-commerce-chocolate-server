15f13fc9302e00eff9c2551f87ef1c928df426d4
MuhammadBorhan <0317371981@student.wub.edu.bd>
2023-06-04 10:48:46 +0600

    visitor middleware created

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const geoip = require("geoip-lite");
 const cors = require("cors");
 const path = require("path");
 
@@ -9,6 +10,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+// Middleware to save visitor data
+app.use((req, res, next) => {
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const geo = geoip.lookup(ip);
+
+  const visitor = new Visitor({
+    ip: ip,
+    isp: req.headers["isp"],
+    country: geo ? geo.country : "Unknown",
+    city: geo ? geo.city : "Unknown",
+  });
+
+  visitor
+    .save()
+    .then(() => {
+      next();
+    })
+    .catch((error) => {
+      console.error("Error saving visitor:", error);
+      next();
+    });
+});
+
 // routes
 const regionRoute = require("./routes/Region.route");
 const userRoute = require("./routes/user.route");
@@ -17,6 +41,7 @@ const brandRoute = require("./routes/Brand.route");
 const giftBoxRoute = require("./routes/GiftBox.route");
 const trendGiftRoute = require("./routes/TrendGift.route");
 const eventRoute = require("./routes/Event.route");
+const Visitor = require("./models/Visitor");
 
 // Home page
 app.get("/", (req, res) => {
