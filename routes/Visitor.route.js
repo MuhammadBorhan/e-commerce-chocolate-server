d099593543d5b1f517fdb63e3e0635944ccd5a1a
MuhammadBorhan <0317371981@student.wub.edu.bd>
2023-06-04 13:54:16 +0600

    get ip and isp

--- routes/Visitor.route.js
+++ routes/Visitor.route.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const visitorController = require("../controllers/Visitor.controller");
 
-router.use(visitorController.saveVisitor);
+// router.use(visitorController.saveVisitor);
+router.post("/visitors", visitorController.saveVisitor);
 router.get("/visitors", visitorController.getVisitors);
 
 module.exports = router;
