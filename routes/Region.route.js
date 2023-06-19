7af00691b584b722e6d767f55312bb7d27268268
MuhammadBorhan <0317371981@student.wub.edu.bd>
2023-06-19 12:07:48 +0600

    api secured done for region

--- routes/Region.route.js
+++ routes/Region.route.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const regionController = require("../controllers/region.controller");
+const verifyToken = require("../middleware/verifyToken");
+const authorization = require("../middleware/authorization");
 
 router.route("/regions").get(regionController.getRegions);
 router
   .route("/region")
   .get(regionController.getRegionsDistrict)
-  .post(regionController.postRegion);
+  .post(verifyToken, authorization("admin"), regionController.postRegion);
 router
   .route("/region/:id")
   .get(regionController.getRegionById)
-  .patch(regionController.updateRegion)
-  .delete(regionController.deleteRegion);
+  .patch(verifyToken, authorization("admin"), regionController.updateRegion)
+  .delete(verifyToken, authorization("admin"), regionController.deleteRegion);
 
 module.exports = router;
